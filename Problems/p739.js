9593e17a1ae377dc476f274f6ce6bc4b6c808c21
guo <1410131029@qq.com>
2019-01-25 14:33:25 +0800

    739.daily-temperatures

--- Problems/p739.js
+++ Problems/p739.js
@@ -2,7 +2,26 @@
  * @param {number[]} T
  * @return {number[]}
  */
-var dailyTemperatures = function(T) {
-
+var dailyTemperatures = function (T) {
+    let valueStack = [];
+    let recordStack = Array.from({
+        length: T.length
+    }, x => 0);
+    for (let i = 0; i < T.length; i++) {
+        while (valueStack.length > 0 && T[i] > valueStack[valueStack.length - 1].val) {
+            let j = valueStack[valueStack.length - 1].index;
+            valueStack.pop();
+            recordStack[j] = i - j;
+        }
+        valueStack.push(
+            {
+                val: T[i],
+                index: i
+            }
+        );
+    }
+    return recordStack;
 };
-console.log(dailyTemperatures(73, 74, 75, 71, 69, 72, 76, 73));
+
+var temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
+console.log(dailyTemperatures(temperatures));

7b6167df4ac8433e3fdea9007bf94955f9283cc0
guo <1410131029@qq.com>
2019-01-29 16:46:36 +0800

    commit

--- Problems/p136.js
+++ Problems/p136.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+//零和任何数异或都等于任何数, 一个数异或两次就等于0, 又本题中除一个之外每个元素都出现两次 所以用循环异或所有数就等于 只出现一次的那个数 .
 var singleNumber = function(nums) {
     let res=0;
     for(let i=0;i<nums.length;i++)

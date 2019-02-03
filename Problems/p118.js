ca553d739fba9c0d3ce246efdd003fd4dadba294
CorleoneG <1410131029@qq.com>
2019-02-03 20:05:04 +0800

    pascals-triangle-ii submit

--- Problems/p118.js
+++ Problems/p118.js
@@ -2,24 +2,19 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {
-    let nums=[];
-    for (let i=0;i<numRows;i++)
-    {
+var generate = function (numRows) {
+    let nums = [];
+    for (let i = 0; i < numRows; i++) {
         //double array
-        nums[i]=[];
-        for (let j=0;j<=i;j++)
-        {
-           if(j===0||j===i)
-           {
-               nums[i][j]=1;
-           }
-           else
-           {
-               nums[i][j]= nums[i-1][j-1]+nums[i-1][j];
-           }
+        nums[i] = [];
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) {
+                nums[i][j] = 1;
+            } else {
+                nums[i][j] = nums[i - 1][j - 1] + nums[i - 1][j];
+            }
         }
     }
-    return nums ;
+    return nums;
 };
 console.log(generate(5));

0475d99a8d299b70180389efb20c8dc2a58d51ff
CorleoneG <1410131029@qq.com>
2019-02-07 23:54:44 +0800

    reverse-string

---
+++ Problems/p344.js
@@ -0,0 +1,23 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+//数组处理力扣调试和浏览器不一致
+// var reverseString = function(s) {
+//     let data=[];
+//     for(let i=s.length-1;i>=0;i--)
+//     {
+//         data.push(s[i]);
+//     }
+//     return data;
+// };
+//字符串方法
+// var reverseString = function(s) {
+//     let data="";
+//     for(let i=s.length-1;i>=0;i--)
+//     {
+//         data+=s[i];
+//     }
+//     return data;
+// };
+console.log(reverseString(["h","e","l","l","o"]));

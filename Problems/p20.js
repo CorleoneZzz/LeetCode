1ca13446297723619b34947b92416101dc8bf529
Guobinze <1410131029@qq.com>
2020-03-23 16:56:51 +0800

    new solution for p20

--- Problems/p20.js
+++ Problems/p20.js
@@ -2,29 +2,52 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isValid = function(s) {
+//     var valueStack=[];
+//     if(s.length===0)
+//     {
+//         return true;
+//     }
+//     if(s.length%2!==0)
+//     {
+//         return false;
+//     }
+//     for (let i=0;i<s.length;i++)
+//     {
+//         if(s[i]==="("||s[i]==="{"||s[i]==="[")
+//         {
+//             valueStack.push(s[i]);
+//             console.log(s[i]+"进栈，栈为"+valueStack);
+//         }
+//         if((s[i]===")"&&valueStack[valueStack.length-1]==="(")||(s[i]==="}"&&valueStack[valueStack.length-1]==="{")||(s[i]==="]"&&valueStack[valueStack.length-1]==="["))
+//        {
+//            valueStack.pop();
+//            console.log("出栈，栈为"+valueStack)
+//        }
+//     }
+//     return valueStack.length===0;
+// };
 var isValid = function(s) {
-    var valueStack=[];
-    if(s.length===0)
-    {
-        return true;
+    const stack = [];
+    const mapper = {
+        '{': "}",
+        "[": "]",
+        "(": ")"
+    };
+    for (let i in s) {
+        const v = s[i];
+        if (['(', '{', '['].indexOf(v) > -1) {
+            stack.push(v);
+        } else {
+            const peak = stack.pop();
+            if (v !== mapper[peak]) {
+                return false;
+            }
+        }
     }
-    if(s.length%2!==0)
-    {
+    if (stack.length > 0) {
         return false;
     }
-    for (let i=0;i<s.length;i++)
-    {
-        if(s[i]==="("||s[i]==="{"||s[i]==="[")
-        {
-            valueStack.push(s[i]);
-            console.log(s[i]+"进栈，栈为"+valueStack);
-        }
-        if((s[i]===")"&&valueStack[valueStack.length-1]==="(")||(s[i]==="}"&&valueStack[valueStack.length-1]==="{")||(s[i]==="]"&&valueStack[valueStack.length-1]==="["))
-       {
-           valueStack.pop();
-           console.log("出栈，栈为"+valueStack)
-       }
-    }
-    return valueStack.length===0;
+    return true;
 };
 console.log(isValid("{[]}"));
---
+++ Problems/p438.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    let n = s.length;
+    let l = p.length;
+    let vp = new Array(128).fill(0);
+    let vs = new Array(128).fill(0);
+    let ans = [];
+    for (let c of p) {
+        vp[c.charCodeAt()]++;
+    }
+    for (let i = 0; i < n; i++) {
+        if () {
+
+        }
+    }
+    return ans;
+};
+var h =findAnagrams("cbaebabacd","abc")
+console.log(h);
